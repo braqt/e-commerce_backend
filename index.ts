bde081f9b7d9c8c2cb0fbc74358f5615145d3252
braqt <brian.quinteros216@gmail.com>
2023-12-01 00:27:27 -0300

    Refactor

--- index.ts
+++ index.ts
@@ -1,28 +1,29 @@
 require("dotenv").config();
+import { initializeApp, cert } from "firebase-admin/app";
 
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 
+import envVars from "./src/constants/config";
+
 import ProductsRoutes from "./src/routes/products";
 import OrderRoutes from "./src/routes/orders";
 import UserRoutes from "./src/routes/users";
 import AdminRoutes from "./src/routes/admin";
-import { initializeApp, cert } from "firebase-admin/app";
 
-const { PORT, MONGO_URI, CERT } = process.env;
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 mongoose.set("strictQuery", false);
-mongoose.connect(MONGO_URI, {
+mongoose.connect(envVars.MONGO_URI, {
   dbName: "ecommerce",
 });
 
 initializeApp({
-  credential: cert(JSON.parse(CERT)),
+  credential: cert(JSON.parse(envVars.CERT)),
 });
 
 app.use("/products", ProductsRoutes);
@@ -30,4 +31,4 @@ app.use("/orders", OrderRoutes);
 app.use("/users", UserRoutes);
 app.use("/admin", AdminRoutes);
 
-app.listen(PORT, () => console.log("Listening on Port:", PORT));
+app.listen(envVars.PORT, () => console.log("Listening on Port:", envVars.PORT));
